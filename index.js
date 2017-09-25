7db70559788e3149a4b09ebfb35815ef140ca0e9
Peter van Ooy <pevanooy@gmail.com>
2017-09-25 19:14:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 function nestedTarget(){
-
+  return document.getElementById('app').querySelector('div div div div div')
 }
 function increaseRankBy(n){
 
