7c5b065b80aff1e318b7b8b20d401d642b8a629d
Peter van Ooy <pevanooy@gmail.com>
2017-09-25 19:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ function nestedTarget(){
   return document.getElementById('app').querySelector('div div div div div')
 }
 function increaseRankBy(n){
-
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+   
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString()
+  }
 }
 function deepestChild(){
 
